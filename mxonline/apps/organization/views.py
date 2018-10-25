591950cde7ba41bc43dc0520850abb57ebfde12f
zhang ying <emailzhangy@163.com>
2018-10-25 23:22:14 +0800

    课程过滤

--- mxonline/apps/organization/views.py
+++ mxonline/apps/organization/views.py
@@ -11,22 +11,33 @@ class OrgView(View):
         all_orgs = CourseOrg.objects.all()
         org_nums = all_orgs.count()
         all_cities = CityDict.objects.all()
+
+        # 城市筛选
+        city_id = request.GET.get("city", "")
+        if city_id:
+            all_orgs = all_orgs.filter(city_id=int(city_id))
+
+        # 类别筛选
+        category = request.GET.get('ct', "")
+        if category:
+            all_orgs = all_orgs.filter(category=category)
+
         # 对课程机构进行分页
         try:
             page = request.GET.get('page', 1)
         except PageNotAnInteger:
             page = 1
 
-        objects = ['john', 'edward', 'josh', 'frank']
-
         # Provide Paginator with the request object for complete querystring generation
 
-        p = Paginator(all_orgs, 5, request=request)
+        p = Paginator(all_orgs, 4, request=request)
 
         orgs = p.page(page)
 
         return render(request, "org-list.html", {
             "all_orgs": orgs,
             "all_cities": all_cities,
-            "org_nums": org_nums
+            "org_nums": org_nums,
+            "city_id": city_id,
+            "category": category
         })
